0fdb720029fe960ade28785aabe87b05f534d889
Mina A. Gergis <minaatifabduallah@gmail.com>
2022-03-25 21:55:07 +0200

    Adding the Auth for the project

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 class DashboardController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function getDashboard()
     {
