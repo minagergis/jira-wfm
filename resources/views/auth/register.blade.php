225cbe9375d02269f47d9d454e5dd74fad694dc8
Mina A. Gergis <minaatifabduallah@gmail.com>
2022-03-25 21:58:04 +0200

    Adding the Auth for the project

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
     <meta name="author" content="Creative Tim">
-    <title>WFM - Login</title>
+    <title>WFM - Register</title>
     <!-- Favicon -->
     <link rel="icon" href="{{asset('assets/img/brand/favicon.png')}}" type="image/png">
     <!-- Fonts -->
