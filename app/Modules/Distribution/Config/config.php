d3bb9321502116d65ff87afdb30735e2f1163774
Mina A. Gergis <minaatifabduallah@gmail.com>
2022-05-31 23:30:00 +0200

    manage contact types in jira integration

--- app/Modules/Distribution/Config/config.php
+++ app/Modules/Distribution/Config/config.php
@@ -8,4 +8,6 @@ return [
         'password' => env('JIRA_TOKEN'),
     ],
     'distribution_hour_every_day' => env('DISTRIBUTION_HOUR_EVERY_DAY', '02:00'),
+
+    'distribution_depends_on_priority' => (bool) env('DISTRIBUTION_DEPENDS_ON_PRIORITY', false),
 ];
--- app/Modules/Distribution/Enums/ZendeskTaskPriorityPointsEnum.php
+++ app/Modules/Distribution/Enums/ZendeskTaskPriorityPointsEnum.php
@@ -2,22 +2,23 @@
 
 namespace App\Modules\Distribution\Enums;
 
-
-
 final class ZendeskTaskPriorityPointsEnum
 {
     /* ALL AVAILABLE PRIORITY POINTS */
 
+    public const URGENT  = 'Urgent';
+
     public const HIGH  = 'High';
 
-    public const MEDIUM  = 'Medium';
+    public const NORMAL  = 'Normal';
 
     public const LOW  = 'Low';
 
     public const PRIORITY_POINTS = [
-        self::HIGH     => 20,
-        self::MEDIUM   => 15,
-        self::LOW      => 10,
+        self::URGENT => 30,
+        self::HIGH   => 20,
+        self::NORMAL => 15,
+        self::LOW    => 10,
     ];
 
     /* ALL AVAILABLE PRIORITY POINTS */
