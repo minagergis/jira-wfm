b6478b436db49c9c6b136a25a2a1216e99f9c702
Mina A. Gergis <minaatifabduallah@gmail.com>
2022-06-11 18:36:29 +0200

    - remove the assign shift button - add distribution button to the team list page

--- app/Modules/Teams/Resources/views/index.blade.php
+++ app/Modules/Teams/Resources/views/index.blade.php
@@ -21,6 +21,7 @@
                         </div>
                         <div class="col-lg-6 col-5 text-right">
                             <a href="{{route('get.teams.create')}}" class="btn btn-sm btn-neutral">New</a>
+                            <a href="{{route('get.distribution.distribute')}}" class="btn btn-sm btn-neutral">Distribute Manually</a>
                         </div>
                     </div>
                 </div>
