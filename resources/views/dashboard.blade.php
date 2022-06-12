bba2a613033d73d93e5e93113b1bbe5d3f879285
Mina A. Gergis <minaatifabduallah@gmail.com>
2022-06-12 20:18:26 +0200

    fix some typos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -178,27 +178,12 @@
             <!-- Footer -->
             <footer class="footer pt-0">
                 <div class="row align-items-center justify-content-lg-between">
-                    <div class="col-lg-6">
-                        <div class="copyright text-center text-lg-left text-muted">
-                            &copy; 2022 <a href="https://www.digisay.com" class="font-weight-bold ml-1" target="_blank">Digisay</a>
+                    <div class="col-xl-12">
+                        <div class="copyright text-center text-xl-center text-muted">
+                            &copy; <?php echo date('Y')?> <a href="http://wfm.antipiracy.me" class="font-weight-bold ml-1" target="_blank">Made with love ❤️</a>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                            <li class="nav-item">
-                                <a href="" class="nav-link" target="_blank">Digisay</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="" class="nav-link" target="_blank">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="" class="nav-link" target="_blank">Blog</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="" class="nav-link" target="_blank">License</a>
-                            </li>
-                        </ul>
-                    </div>
+
                 </div>
             </footer>
         </div>
