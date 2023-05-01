6a0812c1816665785b49a9db8e228189297929bc
Mina A. Gergis <minaatifabduallah@gmail.com>
2023-05-01 19:44:03 +0300

    ## [5.0.5] - 2023-05-01 ### Fixed - Daylight saving timing issue in member schedules.

--- app/Modules/Shifts/Config/config.php
+++ app/Modules/Shifts/Config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $suitableTimeZone = ((bool) env('DST_SUMMER_TIME', true)) ? 'Asia/Riyadh' : 'Africa/Cairo';
-dd($suitableTimeZone);
+
 return [
     'name'            => 'Shifts',
     'shifts_timezone' => $suitableTimeZone,
